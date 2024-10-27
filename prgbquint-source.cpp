d97403dec5dd24362542b6b99fe5ec9de3f76636
VeXiFy <vexisawesome1716@outlook.com>
2024-10-27 17:31:30 -0400

    prgbquint source code

---
+++ prgbquint-source.cpp
@@ -0,0 +1,320 @@
+#include <windows.h>
+#include <cmath>
+#include <ctime>
+#include <mmsystem.h>
+#define _USE_MATH_DEFINES 1
+#pragma comment(lib, "winmm.lib")
+typedef union _RGBQUAD {
+    COLORREF rgb;
+    struct {
+        BYTE b;
+        BYTE g;
+        BYTE r;
+        BYTE unused;
+    };
+}_RGBQUAD, *PRGBQUAD;
+
+DWORD WINAPI prgbquads(LPVOID lpParam)
+{ //prgbquad from trichloromethane
+	HDC hdcScreen = GetDC(0), hdcMem = CreateCompatibleDC(hdcScreen);
+	INT w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	BITMAPINFO bmi = { 0 };
+	PRGBQUAD rgbScreen = { 0 };
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	HBITMAP hbmTemp = CreateDIBSection(hdcScreen, &bmi, NULL, (void**)&rgbScreen, NULL, NULL);
+	SelectObject(hdcMem, hbmTemp);
+	for (;;) {
+		hdcScreen = GetDC(0);
+		BitBlt(hdcMem, 0, 0, w, h, hdcScreen, 0, 0, SRCCOPY);
+		for (INT i = 0; i < w * h; i++) {
+			//INT x = i % w, y = i / w;
+			rgbScreen[i].rgb -= 666;
+		}
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+		Sleep(100);
+		ReleaseDC(NULL, hdcScreen); DeleteDC(hdcScreen);
+	}
+}
+
+DWORD WINAPI prgbquads1(LPVOID lpParam)
+{ //prgbquad from salinewin
+	HDC hdcScreen = GetDC(0), hdcMem = CreateCompatibleDC(hdcScreen);
+	INT w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	BITMAPINFO bmi = { 0 };
+	PRGBQUAD rgbScreen = { 0 };
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	HBITMAP hbmTemp = CreateDIBSection(hdcScreen, &bmi, NULL, (void**)&rgbScreen, NULL, NULL);
+	SelectObject(hdcMem, hbmTemp);
+	for (;;) {
+		hdcScreen = GetDC(0);
+		BitBlt(hdcMem, 0, 0, w, h, hdcScreen, 0, 0, SRCCOPY);
+		for (INT i = 0; i < w * h; i++) {
+			INT x = i % w, y = i / w;
+			rgbScreen[i].rgb += x + y;
+		}
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+		ReleaseDC(NULL, hdcScreen); DeleteDC(hdcScreen);
+	}
+}
+
+DWORD WINAPI prgbquads2(LPVOID lpParam) { //prgbquad from Triphenylarsine, it has hue.
+    int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+    _RGBQUAD* data = (_RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(_RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    for (int i = 0;; i++, i %= 3) {
+        HDC desk = GetDC(NULL);
+        HDC hdcdc = CreateCompatibleDC(desk);
+        HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+        SelectObject(hdcdc, hbm);
+        BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+        GetBitmapBits(hbm, w * h * 4, data);
+        for (int i = 0; w * h > i; i++) {
+            data[i].rgb = (data[i].rgb * 2) % (RGB(255, 255, 255));
+        }
+        SetBitmapBits(hbm, w * h * 4, data);
+        BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+        DeleteObject(hbm);
+        DeleteObject(hdcdc);
+        DeleteObject(desk);
+    }
+    return 0;
+}
+
+DWORD WINAPI prgbquads3(LPVOID lpParam)
+{ //xor prgbquad
+	HDC hdcScreen = GetDC(0), hdcMem = CreateCompatibleDC(hdcScreen);
+	INT w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	BITMAPINFO bmi = { 0 };
+	PRGBQUAD rgbScreen = { 0 };
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	HBITMAP hbmTemp = CreateDIBSection(hdcScreen, &bmi, NULL, (void**)&rgbScreen, NULL, NULL);
+	SelectObject(hdcMem, hbmTemp);
+	for (;;) {
+		hdcScreen = GetDC(0);
+		BitBlt(hdcMem, 0, 0, w, h, hdcScreen, 0, 0, SRCCOPY);
+		for (INT i = 0; i < w * h; i++) {
+			INT x = i % w, y = i / w;
+			rgbScreen[i].rgb += x ^ y;
+		}
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+		ReleaseDC(NULL, hdcScreen); DeleteDC(hdcScreen);
+	}
+}
+
+DWORD WINAPI prgbquads4(LPVOID lpParam)
+{
+    int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+    _RGBQUAD* data = (_RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(_RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    for (int i = 0;; i++, i %= 3) {
+        HDC desk = GetDC(NULL);
+        HDC hdcdc = CreateCompatibleDC(desk);
+        HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+        SelectObject(hdcdc, hbm);
+        BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+        GetBitmapBits(hbm, w * h * 4, data);
+        for (int i = 0; w * h > i; i++) {
+            int x = i % w, y = i / h;
+            data[i].rgb -= x & y;
+        }
+        SetBitmapBits(hbm, w * h * 4, data);
+        BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+        DeleteObject(hbm);
+        DeleteObject(hdcdc);
+        DeleteObject(desk);
+    }
+    return 0;
+}
+
+DWORD WINAPI prgbquads5(LPVOID lpParam)
+{
+    int time = GetTickCount();
+    int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+    RGBQUAD* data = (RGBQUAD*)VirtualAlloc(0, (w * h + w) * sizeof(RGBQUAD), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    for (int i = 0;; i++, i %= 3) {
+        HDC desk = GetDC(NULL);
+        HDC hdcdc = CreateCompatibleDC(desk);
+        HBITMAP hbm = CreateBitmap(w, h, 1, 32, data);
+        SelectObject(hdcdc, hbm);
+        BitBlt(hdcdc, 0, 0, w, h, desk, 0, 0, SRCCOPY);
+        GetBitmapBits(hbm, w * h * 4, data);
+        int v = 0;
+        BYTE byte = 0;
+        if ((GetTickCount() - time) > 60000)
+            byte = rand()%0xff;
+        for (int i = 0; w * h > i; i++) {
+            if (i % h && rand() % 110)
+                v = rand() % 100;
+            *((BYTE*)data + 4 * i + v) -= 5;
+        }
+        SetBitmapBits(hbm, w * h * 4, data);
+        BitBlt(desk, 0, 0, w, h, hdcdc, 0, 0, SRCCOPY);
+        DeleteObject(hbm);
+        DeleteObject(hdcdc);
+        DeleteObject(desk);
+    }
+    return 0;
+} 
+
+VOID WINAPI afx1() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 16000, 16000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[16000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>((t>>5)*(t>>3)|t>>4);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+VOID WINAPI afx2() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(9*t&t>>4|5*t&t>>7|(3*t&t>>10)-1);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+VOID WINAPI afx3() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(10*(t>>7|t|t>>(t>>15))+(7&t>>11)|t>>2);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+
+VOID WINAPI afx4() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(t*(0xca98>>(t>>9&14)&15)|t>>(t&6144?3:2));
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+VOID WINAPI afx5() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 11000, 11000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[11000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(t*(t/1024^t)|t>>2);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+VOID WINAPI afx6() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[8000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>(65*sin(((t>>9^(t>>9)-1^1)%13*t)*32));
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+
+
+
+
+int main () {
+	if (MessageBoxW(NULL, L"PrgbQuint.exe\r\nTubercomiosis's new PRGBQUAD-only malware\r\n\r\nBy running this, you are 100% sure you don't have epilepsy or heart problems.\r\nDo you want to run it?", L"PrgbQuint.exe - EPILEPSY WARNING!", MB_YESNO | MB_ICONWARNING) == IDNO)
+	{
+		ExitProcess(0);
+	}
+	else
+	{
+		// this is skidded
+		if (MessageBoxW(NULL, L"(This is skidded btw)\r\nARE YOU REALLY SURE YOU WANT TO RUN THIS?\r\n\r\nWHEN YOU RUN IT, I'M NOT RESPONSIBLE FOR GIVING YOU A SEIZURE!\r\nDO YOU STILL WANT TO RUN IT ANYWAY?", L"PrgbQuint.exe - OH NO!!!!!!!", MB_YESNO | MB_ICONWARNING) == IDNO)
+		{
+			ExitProcess(0);
+		}
+		else
+		{
+			Sleep(5000);
+			MessageBoxW(NULL, L"Terrible mistake!", L"Terrible mistake!", MB_OK | MB_ICONERROR);
+			HANDLE a = CreateThread(0, 0, prgbquads, 0, 0, 0);
+			afx1();
+			Sleep(30000);
+			TerminateThread(a, 0);
+			CloseHandle(a);
+			InvalidateRect(0, 0, 0);
+			Sleep(1000);
+			HANDLE b = CreateThread(0, 0, prgbquads1, 0, 0, 0);
+			afx2();
+			Sleep(30000);
+			TerminateThread(b, 0);
+			CloseHandle(b);
+			InvalidateRect(0, 0, 0);
+			Sleep(1000);
+			HANDLE c = CreateThread(0, 0, prgbquads2, 0, 0, 0);
+			afx3();
+			Sleep(30000);
+			TerminateThread(c, 0);
+			CloseHandle(c);
+			InvalidateRect(0, 0, 0);
+			Sleep(1000);
+			HANDLE d = CreateThread(0, 0, prgbquads3, 0, 0, 0);
+			afx4();
+			Sleep(30000);
+			TerminateThread(d, 0);
+			CloseHandle(d);
+			InvalidateRect(0, 0, 0);
+			Sleep(1000);
+			HANDLE e = CreateThread(0, 0, prgbquads4, 0, 0, 0);
+			afx5();
+			Sleep(30000);
+			TerminateThread(e, 0);
+			CloseHandle(e);
+			InvalidateRect(0, 0, 0);
+			Sleep(1000);
+			HANDLE f = CreateThread(0, 0, prgbquads5, 0, 0, 0);
+			afx6();
+			Sleep(30000);
+			ExitProcess(0);
+		}
+	}
+}
